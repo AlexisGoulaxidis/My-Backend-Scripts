e51a3a4f3154503bd81f1ac544b444d5160dbe50
AlexisGoulaxidis <97764007+AlexisGoulaxidis@users.noreply.github.com>
2022-05-19 20:20:28 +0300

    Update TCP_Socket.py
    I created simple TCP-like Echo connection

--- TCP_Socket.py
+++ TCP_Socket.py
@@ -1,1 +1,19 @@
+import socket
 
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+	  s.bind(('127.0.0.1', 4444))
+	  s.listen(10) #max connections
+  
+	  while True:
+    
+			conn, adrr = s.accept()
+			data = conn.recv(1024) #max len
+    
+			if not data or data.decode() == 'close': break
+			
+			conn.send(data) #return accepted data
+			
+			#auto-close connection
+    
+    
